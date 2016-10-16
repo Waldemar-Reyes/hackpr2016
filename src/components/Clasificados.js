544ab32cc682bf1367bcc98f57b66102ce60a05b
Leandro Rios <Leandro Rios>
2016-10-16 04:35:43 -0400

    clasificados con css y componentes de clasificados

--- src/components/Clasificados.js
+++ src/components/Clasificados.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import {guitarra} from '../../webpages/clasificadosReact.js';
+import '../../style/clasificadosPhoto.scss';
 
 class Clasificados extends Component {
   constructor() {
@@ -8,7 +8,15 @@ class Clasificados extends Component {
   }
 
   render() {
-    return null;
+    return (
+<div>
+< img className="clasificados" src="https://c2.staticflickr.com/6/5640/30055029760_0e3391942d_o.png" alt="background image"/>
+    <a href="login.html" target="_blank">
+      <img className="moveimage" src="https://c2.staticflickr.com/6/5746/30344147985_b48efacb9c_o.png" alt="ATH Movil Logo"/>
+    </a>
+</div>
+    	);
+      
   }
 }
 
