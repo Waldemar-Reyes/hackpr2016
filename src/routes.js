10a8ddbd34bf0289e4bd967314a56bcf78050659
Leandro Rios <Leandro Rios>
2016-10-16 01:01:31 -0400

    Updated routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Route, Router, browserHistory, IndexRoute } from 'react-router';
 import App from './components/App';
 import Checkout from './components/Checkout';
-import Clasificados from './components/Checkout';
-import SignIn from './components/Checkout';
+import Clasificados from './components/Clasificados';
+import SignIn from './components/SignIn';
 import APITest from './components/APITest';
 
 export default (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={APITest}/>
+      <Route component={APITest}/>
       <Route component={Clasificados}/>
       <Route path="checkout" component={Checkout}/>
-      <Route path="signIn" component={SignIn}/>
+      <IndexRoute component={SignIn}/>
     </Route>
   </Router>
 );
